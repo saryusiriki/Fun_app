5aa6dc8bb10a11e2b9d40a387f6562d8b7df8539
saryu <119947252+saryusiriki@users.noreply.github.com>
2023-02-27 23:25:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hello there:sunglasses:")
+st.header(":blue[I am saryu siriki]")
+st.subheader("Not your usual data science enthusiast. I am a total data fanatic; with so much data available, deriving insights from data is challenging, yet fun. I enjoy bringing mathematical solutions to problems. Since data science and machine learning use math extensively, I enjoy exploring and studying data even more. I also like working on artificial neural networks and natural language processing. I code primarily in Python. ")
+
